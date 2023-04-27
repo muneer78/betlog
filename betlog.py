045c44d814bb3880f84858d01b90a99e1fc03788
muneer78 <muneer@duck.com>
2023-04-26 20:45:47 -0500

    Added Total ROI to analytics file and updated gamblerz file to show total winning percentage

--- betlog.py
+++ betlog.py
@@ -54,7 +54,8 @@ wins = (df.Result.str.count(substr1).sum())
 substr2 = 'L'
 losses = (df.Result.str.count(substr2).sum())
 
-squareroot = float((wins + losses) ** (1/2))
+# squareroot = float((wins + losses) ** (1/2))
+squareroot = np.sqrt((wins + losses))
 
 gamblerz = (wins - losses)/squareroot
 gamblerz = gamblerz.round(2)
@@ -62,13 +63,16 @@ gamblerz_str = str(gamblerz)
 p_value = scipy.stats.norm.sf(abs(gamblerz))
 p_value = p_value.round(2)
 p_value_str = str(p_value)
+winning_pct = (wins / (wins + losses)) *100
+winning_pct = winning_pct.round(2)
+win_pct_str = str(winning_pct)
 
 # This line opens a file named "student.txt" in write mode (w)
 file = open("gamblerz.txt", "w+")
 
 # This line converts the variables to a string using the str() function and writes it to the file
-file.write(str("The Gambler Z-Score is " + gamblerz_str + "\n"))
-file.write(str("The P-value is " + p_value_str + "\n"))
+file.write(str("Your Gambler Z-Score is " + gamblerz_str + "\n"))
+file.write(str("Your winning percentage is " + win_pct_str + "%" + "\n"))
 
 # This line closes the "student.txt" file
 file.close()
@@ -86,10 +90,23 @@ df10 = df.groupby(['BetType'])['Amount'].sum().reset_index().round(2)
 df11 = df.groupby(['Sport'])['Amount'].sum().reset_index().round(2)
 df12 = df.groupby(['Sportsbook'])['Amount'].sum().reset_index().round(2)
 
+# Sum column values for A, B and C
+sum_a = df['ActualPayout'].sum()
+sum_b = df['Amount'].sum()
+
+# Write only the sums to new data frame
+df13 = pd.DataFrame({'TotalWon':sum_a, 'TotalRisked':sum_b}, index=[0])
+
+# Set the index of the new DataFrame
+# df13.index = [0,1,2]
+
+# Divide sum of A by sum of B
+df13['TotalROI'] = (df13['TotalWon'] / df13['TotalRisked'] * 100).round(2)
+
 for df in [df9, df10, df11, df12]:
     df.rename(columns={'Amount': 'MoneyRisked'}, inplace=True)
 
-list_of_dfs = [df3, df4, df5, df6, df7, df8, df9, df10, df11, df12]
+list_of_dfs = [df3, df4, df5, df6, df7, df8, df9, df10, df11, df12, df13]
 with open('analytics.csv','w+') as f:
     for df in list_of_dfs:
         df.to_csv(f, index=False)
