3112d0cd86dff2e28e0f3169fe76a16e9ae3bcd3
jacquesfunk <reenum@gmail.com>
2024-11-11 21:22:35 -0600

    added script

---
+++ implied-prob.py
@@ -0,0 +1,14 @@
+from rich import print
+
+def implied_probability(odds):
+    """Convert odds to implied probability"""
+    if odds < 0:
+        return abs(odds) / (abs(odds) + 100)
+    else:
+        return 100 / (odds + 100)
+
+odds = float(input("Enter odds: "))
+
+implied_prob = implied_probability(odds) * 100
+
+print(f"The implied probability is {implied_prob:.2f}%")
