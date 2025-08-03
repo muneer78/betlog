d02318623f2f62a75a4bfcdecab78193e6aaafef
muneer78 <muneer78@MacBook-Pro.local>
2025-08-03 08:31:38 -0500

    deleted fantasy sports scripts

--- draft-order-v1.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-places = ['10th', '9th', '8th', '7th', '6th', '5th', '4th', '3rd', '2nd', '1st']
-np.random.shuffle(places)
-print(places)
-# sampleNumbers = np.random.shuffle(places, 10, p=[0.25, 0.2, 0.15, 0.12, 0.1, 0.07, 0.05, 0.045, 0.01, 0.005])
